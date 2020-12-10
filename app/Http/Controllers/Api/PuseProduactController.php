0efef6f2af2f4eb1f4b6fdc021d8ad025509f6d7
fox_it <fox_it@yahoo.com>
2020-12-10 18:11:51 +0200

    Editing

--- app/Http/Controllers/Api/PuseProduactController.php
+++ app/Http/Controllers/Api/PuseProduactController.php
@@ -2,8 +2,9 @@
 
 namespace App\Http\Controllers\Api;
 
-use App\Http\Controllers\Controller;
+use App\Model\Produact;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
 
 class PuseProduactController extends Controller
 {
