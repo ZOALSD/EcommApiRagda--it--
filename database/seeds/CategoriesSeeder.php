665d99e6a2dd7f393d00f10e256ea8d47d7df93e
fox_it <fox_it@yahoo.com>
2020-12-10 13:44:01 +0200

    Edit Categorise Seeder

--- database/seeds/CategoriesSeeder.php
+++ database/seeds/CategoriesSeeder.php
@@ -12,6 +12,38 @@ class CategoriesSeeder extends Seeder
      */
     public function run()
     {
-        factory(Categories::class ,10)->create();
+        //factory(Categories::class ,10)->create();
+
+        Categories::create([
+            'name' => 'موبايلات' ,
+            'image_cate' => 'CategoriesImage/default.jpg'
+        ]);
+
+        Categories::create([
+            'name' => 'ملابس' ,
+            'image_cate' => 'CategoriesImage/default.jpg'
+        ]);
+
+
+        Categories::create([
+            'name' => 'عربات' ,
+            'image_cate' => 'CategoriesImage/default.jpg'
+        ]);
+
+
+        Categories::create([
+            'name' => 'اواني منزلية' ,
+            'image_cate' => 'CategoriesImage/default.jpg'
+        ]);
+
+        Categories::create([
+            'name' => 'اثاثات' ,
+            'image_cate' => 'CategoriesImage/default.jpg'
+        ]);
+
+        Categories::create([
+            'name' => 'طعام' ,
+            'image_cate' => 'CategoriesImage/default.jpg'
+        ]);
     }
 }
