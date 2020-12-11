63b0e87e4bc73d8646a64578091a56041a093735
fox_it <fox_it@yahoo.com>
2020-12-11 11:03:08 +0200

    Add api for color Route

--- routes/api.php
+++ routes/api.php
@@ -40,7 +40,8 @@ Route::group(['middleware' => 'auth:sanctum'], function () {
     Route::get('size','Api\SizeControllerApi@index');
 
     //For التاجر
-    Route::get('ProEcommPused','Api\ProduactCoontrollerApi@ProEcommPused');//ProEcommPused
+    Route::get('ProEcommPused','Api\ProduactCoontrollerApi@ProEcommPused');//ProEcommColor
+    Route::get('ProEcommColor','Api\ProduactCoontrollerApi@ProEcommColor');//
     Route::get('ProEcomm','Api\ProduactCoontrollerApi@ProEcomm');//
     Route::delete('ProEcommDelete/{id}','Api\ProduactCoontrollerApi@destroy');
     Route::post('AddProduact','Api\ProduactCoontrollerApi@store');
