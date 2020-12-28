39d8bdff1bbf6d93ff00873f09b6743769589bdb
fox <fox_it@yahoo.com>
2020-12-28 13:31:55 +0200

    Change Post Show Card to Get

--- routes/api.php
+++ routes/api.php
@@ -61,6 +61,6 @@ Route::group(['middleware' => 'auth:sanctum'], function () {
     Route::post('card', 'Api\CardControllerApi@card'); //
     Route::post('cardconfirm', 'Api\CardControllerApi@cardconfirm'); //
     Route::post('cardcancle', 'Api\CardControllerApi@cardcancle'); //
-    Route::post('showCard', 'Api\CardControllerApi@showCard'); //
+    Route::get('showCard', 'Api\CardControllerApi@showCard'); //
 
 });
