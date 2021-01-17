72c5aab796872c2a840e6ca19f1e1636e95250aa
Ahmed S3D <fox_it@yahoo.com>
2021-01-17 09:24:36 +0200

    demp DAat

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,5 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
-
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -11,38 +9,38 @@ use Illuminate\Support\Facades\Route;
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
 |
-*/
+ */
 
 // Route::get('/', function () {
 //     return view('welcome');
+// // });
+
+// Route::get('qr-code', function ()
+// {
+//   $pig = QRCode::text('QR Code Generator for Laravel!')->png();
+
 // });
 
-Route::get('qr-code', function () 
-{
-  $pig = QRCode::text('QR Code Generator for Laravel!')->png();   
-   
-});
+// Route::get('qr-code/examples/wifi', function ()
+// {
+//     $authenticationType = "WPA";
+//     $ssId = "ZOOLS3D";
+//     $ssIdisHidden = false;
+//     $password = "ZOOL@777";
 
-Route::get('qr-code/examples/wifi', function () 
-{
-    $authenticationType = "WPA";
-    $ssId = "ZOOLS3D";
-    $ssIdisHidden = false;
-    $password = "ZOOL@777";
-  
-    return QRCode::wifi($authenticationType, $ssId, $password, $ssIdisHidden)
-              ->setOutfile('QRcode/my-wii.png')
-              ->png();
-}); 
+//     return QRCode::wifi($authenticationType, $ssId, $password, $ssIdisHidden)
+//               ->setOutfile('QRcode/my-wii.png')
+//               ->png();
+// });
 
-Route::get('QRCode', function () 
-{
-    $q="ZOOLSD";
-      //save qrcode image in our folder on this site
-      $file = 'generated_qrcodes/'.$q.'.png'; 
-      $newQrcode = QRCode::text()
-       ->setSize(8)
-       ->setMargin(2)
-       ->setOutfile($file)
-       ->png();
-});    
+// Route::get('QRCode', function ()
+// {
+//     $q="ZOOLSD";
+//       //save qrcode image in our folder on this site
+//       $file = 'generated_qrcodes/'.$q.'.png';
+//       $newQrcode = QRCode::text()
+//        ->setSize(8)
+//        ->setMargin(2)
+//        ->setOutfile($file)
+//        ->png();
+// });
