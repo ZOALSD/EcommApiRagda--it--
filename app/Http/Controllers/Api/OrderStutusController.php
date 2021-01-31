ddafa3c32a4351f999aae0864e4db469fce1c2ff
Ahmed S3D <fox_it@yahoo.com>
2021-01-31 17:32:39 +0200

    Stutus Edit IF

--- app/Http/Controllers/Api/OrderStutusController.php
+++ app/Http/Controllers/Api/OrderStutusController.php
@@ -14,11 +14,16 @@ class OrderStutusController extends Controller
 
         $order = CardData::where('clint_id', Auth::id())->where('deliver_stutus', '!=', 1)->first();
 
-        if ($order->order_stutus == 0) {
-            $st = 'الطلب قيد التوصيل';
-        } else {
-            $st = 'الطلب قيد التحضير';
+        if ($order != null) {
+            if ($order->order_stutus == 0) {
+                $st = 'الطلب قيد التوصيل';
+            } else {
+                $st = 'الطلب قيد التحضير';
 
+            }
+        } else {
+            $st = "لا يوجد طلب";
+            return response()->json($st, 200);
         }
 
         return response()->json($st, 200);
